98e42924c3d7ecaa796c50d861eda4d538dc46ca
Guilherme Cremasco <guigulmini@gmail.com>
2026-07-15 20:01:20 -0300

    Removidos templates do Weather Forecast que vêm por padrão em APIs ASP.NET Core.

--- DioMinimalAPI/Program.cs
+++ DioMinimalAPI/Program.cs
@@ -33,25 +33,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.MapPost("/login", (
     [FromBody] LoginDTO loginDTO,
     [FromServices] IAdministradorServico administradorServico) =>
@@ -66,8 +47,3 @@ app.UseSwagger();
 app.UseSwaggerUI();
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
