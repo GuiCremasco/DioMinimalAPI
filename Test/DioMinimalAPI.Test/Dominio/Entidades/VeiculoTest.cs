2d9923fb0c4cdfba43e274782cd936cc1aa4a39f
Guilherme Cremasco <guigulmini@gmail.com>
2026-08-06 20:30:47 -0300

    Criados testes unitários para validar modelos de entidades.

---
+++ Test/DioMinimalAPI.Test/Dominio/Entidades/VeiculoTest.cs
@@ -0,0 +1,26 @@
+﻿using DioMinimalAPI.Dominio.Entidades;
+
+namespace DioMinimalAPI.Test.Dominio.Entidades;
+
+[TestClass]
+public class VeiculoTest
+{
+    [TestMethod]
+    public void TestarGetSetPropriedades()
+    {
+        // Arrange
+        var veiculo = new Veiculo();
+
+        // Act
+        veiculo.ID = 1;
+        veiculo.Nome = "Carro Teste";
+        veiculo.Marca = "Marca Teste";
+        veiculo.Ano = 2026;
+
+        // Assert
+        Assert.AreEqual(1, veiculo.ID);
+        Assert.AreEqual("Carro Teste", veiculo.Nome);
+        Assert.AreEqual("Marca Teste", veiculo.Marca);
+        Assert.AreEqual(2026, veiculo.Ano);
+    }
+}
